86e508bd33757545900dc2c5a05adf2191635b66
Juan Almonte <almonteestrella@gmail.com>
2023-09-02 22:04:16 -0400

    next-auth no working

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,27 @@
+import Feed from '@components/Feed';
 import React from 'react';
 
 const home = () => {
-    return <div>home</div>;
+    return (
+        <section className='w-full flex-center flex-col'>
+            <h1 className='capitalize head_text text-center'>
+                discover & share
+                <br className='max-md:hidden' />
+                <span className='orange_gradient text-center'>
+                    {' '}
+                    AI-powerd prompts
+                </span>
+            </h1>
+            <p className='text-center desc'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam
+                fugiat sequi cupiditate, veniam quo pariatur explicabo, est
+                cumque quae accusantium enim laboriosam ipsam tempora minus
+                debitis? Harum rem tempore quibusdam?
+            </p>
+
+            <Feed />
+        </section>
+    );
 };
 
 export default home;
